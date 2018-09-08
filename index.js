6369431d466a4f73335be5d3f4f6a72444a0d675
Gerhana Chkartina <chkartina@gmail.com>
2018-09-08 23:47:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase ()
+}
+function whisper(string) {
+  return string.toLowerCase ()
+}
+function logShout() {
+  console.log("HELLO")
+}
+function logWhisper() {
+  console.log("hello")
+}
+function sayHiToGrandma(string) {
+  var lowercase = 'hello!'
+  if (string.toLowerCase () === lowercase ) {
+    console.log("I can't hear you!") }
+  }
+function sayHiToGrandma(string) {
+    var uppercase = 'HELLO!'
+  if (string.toUpperCase () === uppercase) {
+    console.log ("YES INDEED") }
+}
